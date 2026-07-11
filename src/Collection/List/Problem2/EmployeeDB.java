087721e5921de499d38026396390885599537b42
tasleem <syedtasleem5225@gmail.com>
2026-07-11 13:02:00 +0530

    Added List and Sets

---
+++ src/Collection/List/Problem2/EmployeeDB.java
@@ -0,0 +1,38 @@
+package Collection.List.Problem2;
+import java.util.ArrayList;
+
+public class EmployeeDB {
+
+    ArrayList<Employee> list = new ArrayList<>();
+
+    public boolean addEmployee(Employee e) {
+        return list.add(e);
+    }
+
+    public boolean deleteEmployee(int empId) {
+        for (Employee e : list) {
+            if (e.getEmpId() == empId) {
+                list.remove(e);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String showPaySlip(int empId) {
+        for (Employee e : list) {
+            if (e.getEmpId() == empId) {
+                return "Pay Slip\nEmployee ID : " + empId +
+                        "\nSalary : " + e.getSalary();
+            }
+        }
+        return "Employee not found";
+    }
+
+    public void showAllEmployees() {
+        for (Employee e : list) {
+            e.getEmployeeDetails();
+            System.out.println("----------------");
+        }
+    }
+}
---
+++ src/Collection/List/Problem6.java
@@ -0,0 +1,33 @@
+package Collection.List;
+
+import java.util.Vector;
+import Collection.List.Problem2.Employee;
+
+public class Problem6 {
+
+    Vector<Employee> list = new Vector<>();
+
+    public boolean addEmployee(Employee e) {
+        return list.add(e);
+    }
+
+    public boolean deleteEmployee(int empId) {
+        for (Employee e : list) {
+            if (e.getEmpId() == empId) {
+                list.remove(e);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String showPaySlip(int empId) {
+        for (Employee e : list) {
+            if (e.getEmpId() == empId) {
+                return "Pay Slip\nEmployee ID : " + empId +
+                        "\nSalary : " + e.getSalary();
+            }
+        }
+        return "Employee not found";
+    }
+}
